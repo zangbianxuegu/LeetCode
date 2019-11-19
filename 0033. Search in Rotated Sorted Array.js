4a28a3653cf51d5fe3bd8b756ea8a234b6d1f8c0
chenzhao <chenzhao@ccbscf.com>
2019-11-19 17:14:46 +0800

    update 0033

--- 0033. Search in Rotated Sorted Array.js
+++ 0033. Search in Rotated Sorted Array.js
@@ -30,3 +30,38 @@ var search = function(nums, target) {
 }
 // Runtime: 56 ms, faster than 69.69% of JavaScript online submissions for Search in Rotated Sorted Array.
 // Memory Usage: 33.8 MB, less than 53.85% of JavaScript online submissions for Search in Rotated Sorted Array.
+
+
+// 2) 二分查找
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let l = 0
+  let r = nums.length - 1
+  while (l <= r) {
+    // 二分数组，根据有序的一半进行查找
+    let m = ~~((l + r) / 2)    
+    if (nums[m] === target) {
+      return m
+    }
+    if (nums[l] <= nums[m]) { // 左边是有序的      
+      if (nums[l] <= target && nums[m] >= target) { // 目标在左边
+        r = m - 1
+      } else { // 目标在右边
+        l = m + 1
+      }
+    } else { // 右边是有序的      
+      if (nums[m] <= target && nums[r] >= target) { // 目标在右边
+        l = m + 1        
+      } else { // 目标在左边
+        r = m - 1
+      }
+    }
+  }
+  return -1
+}
+// Runtime: 80 ms, faster than 6.11% of JavaScript online submissions for Search in Rotated Sorted Array.
+// Memory Usage: 33.9 MB, less than 42.31% of JavaScript online submissions for Search in Rotated Sorted Array.
