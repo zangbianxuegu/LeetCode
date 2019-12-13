d2024c920b7316e0ad0fa43a18b67e9982e87e0f
chenzhao <chenzhao@ccbscf.com>
2019-12-13 19:54:05 +0800

    feat(#0075): add solutions: two pointers

--- 0075. Sort Colors.js
+++ 0075. Sort Colors.js
@@ -53,6 +53,7 @@ const sortColors = (nums) => {
 // Runtime: 48 ms, faster than 95.86% of JavaScript online submissions for Sort Colors.
 // Memory Usage: 33.8 MB, less than 35.71% of JavaScript online submissions for Sort Colors.
 
+
 // 2) 数组上切割
 // 一次循环
 // 时间复杂度 O(n)
@@ -79,3 +80,31 @@ const sortColors = (nums) => {
 }
 // Runtime: 60 ms, faster than 38.37% of JavaScript online submissions for Sort Colors.
 // Memory Usage: 34.1 MB, less than 7.14% of JavaScript online submissions for Sort Colors.
+
+
+// 3) 双指针
+// 两个指针分别指向 0 的结束（0 队列之外）位置和 2 的开始位置（2 队列之外），循环遍历，如果为 0，交换当前元素 0 和 0 指针位置元素，0 指针位置右移，如果为 2，交换当前元素 2 和 2 指针位置元素，2 指针位置左移，此时需要注意，因为元素的交换，换来一个新的元素，下一次循环遍历仍应当前位置开始，而不是 ++。
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const sortColors = (nums) => {
+  let len = nums.length
+  let redPos = 0
+  let bluePos = len - 1
+  const swap = (i, j) => [nums[i], nums[j]] = [nums[j], nums[i]]
+  for (let i = 0; i <= bluePos; i++) {
+    let num = nums[i]
+    if (num === 0) {
+      swap(i, redPos)
+      redPos++
+    } else if (num === 2) {
+      swap(i, bluePos)
+      bluePos--
+      i--
+    }
+  }
+  return nums
+}
+// Runtime: 76 ms, faster than 6.69% of JavaScript online submissions for Sort Colors.
+// Memory Usage: 34.3 MB, less than 7.14% of JavaScript online submissions for Sort Colors.
