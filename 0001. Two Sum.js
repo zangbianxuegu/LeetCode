5b1b92b54488644882c3ccc8c48386a25bd5af9c
zangbianxuegu <zangbianxuegu@gmail.com>
2019-12-20 22:05:24 +0800

    feat(#0001): add solution: map

--- 0001. Two Sum.js
+++ 0001. Two Sum.js
@@ -10,12 +10,14 @@
 // return [0, 1].
 
 
+// 1) Brute force
+// Time: O(n^2)
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+const twoSum = (nums, target) => {
   for (var i = 0; i < nums.length; i++) {
     for (var j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] == target) {
@@ -24,3 +26,39 @@ var twoSum = function(nums, target) {
     }
   }
 }
+
+
+// 2) map
+// Time: O(n)
+// Space: O(n)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  if (nums.length <= 1) {
+    return []
+  }
+  let map = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (map.hasOwnProperty(nums[i])) {
+      return [map[nums[i]], i]
+    } else {
+      map[target - nums[i]] = i
+    }
+  }
+}
+// Runtime: 60 ms, faster than 72.22 % of JavaScript online submissions for Two Sum.
+// Memory Usage: 34.4 MB, less than 89.67 % of JavaScript online submissions for Two Sum.
+
+// Test case:
+// console.log(twoSum([2, 7, 11, 15], 9))
+// map: {
+//   7: 0,
+//   2: 1,
+//   -2, 2,
+//   6, 3
+// }
+
+
