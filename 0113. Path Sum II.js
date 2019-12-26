d90164081b761dec7b14e026a2ca28b0e960d710
chenzhao <chenzhao@ccbscf.com>
2019-12-26 17:57:59 +0800

    feat(#0113): add #0113 Path Sum II

--- 0113. Path Sum II.js
+++ 0113. Path Sum II.js
@@ -22,3 +22,83 @@
 // ]
 
 
+// 1) 回溯
+// https://leetcode.com/problems/path-sum-ii/discuss/36685/12ms-11-lines-C%2B%2B-Solution
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+const pathSum = (root, sum) => {
+  function findPaths(node, sum, path, paths) {
+    if (!node) {
+      return
+    }
+    path.push(node.val)
+    if (!node.left && !node.right && sum === node.val) {
+      paths.push([...path]) // 注意此处 push 的是 path 的副本，因为 path 是对象，接下来会改变
+    }
+    findPaths(node.left, sum - node.val, path, paths)
+    findPaths(node.right, sum - node.val, path, paths)
+    path.pop()
+  }
+  let path = []
+  let paths = []
+  findPaths(root, sum, path, paths)
+  return paths
+};
+// Runtime: 64 ms, faster than 94.48% of JavaScript online submissions for Path Sum II.
+// Memory Usage: 37.4 MB, less than 90.00% of JavaScript online submissions for Path Sum II.
+
+// Test case:
+// let root = {
+//   val: 5,
+//   left: {
+//     val: 4,
+//     left: {
+//       val: 11,
+//       left: {
+//         val: 7,
+//         left: null,
+//         right: null
+//       },
+//       right: {
+//         val: 2,
+//         left: null,
+//         right: null
+//       }
+//     },
+//     right: null
+//   },
+//   right: {
+//     val: 8,
+//     left: {
+//       val: 13,
+//       left: null,
+//       right: null
+//     },
+//     right: {
+//       val: 4,
+//       left: {
+//         val: 5,
+//         left: null,
+//         right: null
+//       },
+//       right: {
+//         val: 1,
+//         left: null,
+//         right: null
+//       }
+//     }
+//   }
+// }
+// console.log(pathSum(root, 22))
+
+
