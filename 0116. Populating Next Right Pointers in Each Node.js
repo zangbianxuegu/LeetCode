c0ec238b4e1976a5d6d227afcaa7a53c2368b69f
chenzhao <chenzhao@ccbscf.com>
2019-12-27 14:08:48 +0800

    feat(#0116): another solution use its next pointer

--- 0116. Populating Next Right Pointers in Each Node.js
+++ 0116. Populating Next Right Pointers in Each Node.js
@@ -29,6 +29,8 @@
 
 // 1) BFS
 // Similar: 0102
+// Space: O(n)
+// 思路：广度优先层层遍历，每层入队列出队列，每层除最后一个节点、其他节点设置 next
 /**
  * // Definition for a Node.
  * function Node(val, left, right, next) {
@@ -62,5 +64,86 @@ const connect = (root) => {
 }
 // Runtime: 72 ms, faster than 96.73% of JavaScript online submissions for Populating Next Right Pointers in Each Node.
 // Memory Usage: 41.4 MB, less than 33.33% of JavaScript online submissions for Populating Next Right Pointers in Each Node.
+
+
+// 2) 利用 next
+// Space: O(1)
+// 思路：保存当前层第一个节点，层层遍历，层层设置 next，如果当前节点没有 next，则从当前层第一个节点的左节点开始遍历
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+const connect = (root) => {
+  if (!root) {
+    return null
+  }
+  let cur = root
+  let levelFirst = cur
+  while (cur) {
+    if (cur.left) {
+      cur.left.next = cur.right
+    }
+    if (cur.next) {
+      if (cur.right) { // 最后一层没有子节点了
+        cur.right.next = cur.next.left
+      }
+      cur = cur.next
+    } else {
+      levelFirst = levelFirst.left
+      cur = levelFirst
+    }
+  }
+  return root
+}
+// Runtime: 76 ms, faster than 89.47% of JavaScript online submissions for Populating Next Right Pointers in Each Node.
+// Memory Usage: 41.1 MB, less than 33.33% of JavaScript online submissions for Populating Next Right Pointers in Each Node.
+
+// Test case:
+// let root = {
+//   val: 1,
+//   left: {
+//     val: 2,
+//     left: {
+//       val: 4,
+//       left: null,
+//       right: null,
+//       next: null
+//     },
+//     right: {
+//       val: 5,
+//       left: null,
+//       right: null,
+//       next: null
+//     },
+//     next: null
+//   },
+//   right: {
+//     val: 3,
+//     left: {
+//       val: 6,
+//       left: null,
+//       right: null,
+//       next: null
+//     },
+//     right: {
+//       val: 7,
+//       left: null,
+//       right: null,
+//       next: null
+//     },
+//     next: null
+//   },
+//   next: null
+// }
+// console.log(connect(root))
 
 
