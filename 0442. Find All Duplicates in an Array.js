2fab2cae648d421ad0107b75264523fd33b6de10
zangbianxuegu <zangbianxuegu@gmail.com>
2020-01-04 23:56:11 +0800

    feat(#0442): add #0442 Find All Duplicates in an Array

---
+++ 0442. Find All Duplicates in an Array.js
@@ -0,0 +1,39 @@
+// Given an array of integers, 1 ≤ a[i]≤ n(n = size of array), some elements appear twice and others appear once.
+
+// Find all the elements that appear twice in this array.
+
+// Could you do it without extra space and in O(n) runtime ?
+
+// Example:
+
+// Input:
+
+// [4, 3, 2, 7, 8, 2, 3, 1]
+
+// Output:
+
+// [2, 3]
+
+
+// 1) map
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const findDuplicates = (nums) => {
+  let map = {}
+  let res = []
+  for (let n of nums) {
+    if (map[n]) {
+      map[n]++
+    } else {
+      map[n] = 1
+    }
+    if (map[n] === 2) {
+      res.push(n)
+    }
+  }
+  return res
+}
+// Runtime: 116 ms, faster than 58.42 % of JavaScript online submissions for Find All Duplicates in an Array.
+// Memory Usage: 48.1 MB, less than 50.00 % of JavaScript online submissions for Find All Duplicates in an Array.
