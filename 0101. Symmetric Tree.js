312a828e256ee0ac2552004ffde96aedb07bb49f
chenzhao <chenzhao@ccbscf.com>
2020-05-01 11:48:14 +0800

    feat(#0101): another recursion

--- 0101. Symmetric Tree.js
+++ 0101. Symmetric Tree.js
@@ -16,7 +16,7 @@
 //    \   \
 //    3    3
 
-// 1)
+// 1) 递归
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -30,7 +30,7 @@
  * @return {boolean}
  */
 const isSymmetric = root => {
-  if (!root.left && !root.right) {
+  if (!root || !root.left && !root.right) {
     return true
   }
   const symmetric = (l, r) => {
@@ -53,3 +53,37 @@ const isSymmetric = root => {
   }
   return false
 }
+
+// 2) 递归
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isSymmetric = root => {
+  if (!root) {
+    return true
+  }
+  const isMirror = (l, r) => {
+    if (!l && !r) {
+      return true
+    }
+    if (!l || !r || l.val !== r.val) {
+      return false
+    }
+    return isMirror(l.left, r.right) && isMirror(l.right, r.left)
+  }
+  return isMirror(root.left, root.right)
+}
+
+// [2,97,97,null,47,80,null,-7,null,null,-7]
+// [1,0]
+// [1]
+// []
