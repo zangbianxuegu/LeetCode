8b9c2aabc9d63ffb2ecc13517c496431e83ba881
chenzhao <chenzhao@ccbscf.com>
2020-05-05 23:58:42 +0800

    feat(#0152): 动态规划解法

--- 0152. Maximum Product Subarray.js
+++ 0152. Maximum Product Subarray.js
@@ -37,3 +37,33 @@ const maxProduct = function(nums) {
   }
   return res
 }
+
+// 2) 动态规划
+// similar: 53
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxProduct = function(nums) {
+  if (!nums.length) {
+    return 0
+  }
+  let res = -Infinity
+  let curMin = 1
+  let curMax = 1
+  for (const num of nums) {
+    const min = Math.min(num, curMin * num, curMax * num)
+    const max = Math.max(num, curMin * num, curMax * num)
+    curMin = min
+    curMax = max
+    res = Math.max(res, curMax)
+  }
+  return res
+}
+// const nums = [2, 3, -2, 4]
+// const nums = [2, 3, -2]
+// const nums = [0, 2]
+// const nums = [2]
+// const nums = []
+// const nums = [2, -5, -2, -4, 3]
+// console.log(maxProduct(nums))
