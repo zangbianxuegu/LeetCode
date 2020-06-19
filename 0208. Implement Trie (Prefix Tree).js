e9a81ca7baead7c5cfedf2635d552e93e26c17a5
zangbianxuegu <zangbianxuegu@gmail.com>
2020-06-19 15:39:34 +0800

    feat(0208): prefix tree solution

--- 0208. Implement Trie (Prefix Tree).js
+++ 0208. Implement Trie (Prefix Tree).js
@@ -40,3 +40,47 @@ class Trie {
 }
 // Runtime: 492 ms, faster than 6.10% of JavaScript online submissions for Implement Trie (Prefix Tree).
 // Memory Usage: 47.7 MB, less than 99.88% of JavaScript online submissions for Implement Trie (Prefix Tree).
+
+
+// 2) prefix tree 前缀树
+// 参考 Solution
+// insert: Time: O(m), Space: O(m), m 为 word 的长度
+// search: Time: O(m), Space: O(1)
+// startsWith: Time: O(m), Space: O(1)
+// 1) 的想法还是 too young too simple, 没有考虑 trie 的概念以及应用场景，indexOf 的时间复杂度至少为 O(n), 那么 startsWith 的时间复杂度至少为 O(n^2)，空间复杂度也有差别。
+class Trie {
+  constructor () {
+    this.root = {}
+  }
+  insert (word) {
+    let node = this.root
+    for (const c of word) {
+      if (!node[c]) {
+        node[c] = {}
+      }
+      node = node[c]
+    }
+    node.isEnd = true
+  }
+  _searchPrefix (word) {
+    let node = this.root
+    for (const c of word) {
+      if (!node[c]) {
+        return null
+      }
+      node = node[c]
+    }
+    return node
+  }
+  search (word) {
+    const node = this._searchPrefix(word)    
+    return node !== null && node.isEnd === true
+  }
+  startsWith (word) {
+    const node = this._searchPrefix(word)
+    return node !== null
+  }
+}
+// Runtime: 180 ms, faster than 78.57% of JavaScript online submissions for Implement Trie (Prefix Tree).
+// Memory Usage: 51.4 MB, less than 92.26% of JavaScript online submissions for Implement Trie (Prefix Tree).
+
