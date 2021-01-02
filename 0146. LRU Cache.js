833c2fdf1b12d88057d9e851a8e14fec2219a4fd
zangbianxuegu <zangbianxuegu@gmail.com>
2021-01-02 23:14:00 +0800

    Update 0146. LRU Cache.js
    add solution: double linked list

--- 0146. LRU Cache.js
+++ 0146. LRU Cache.js
@@ -1,4 +1,4 @@
-// esign and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+// Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
 
 // get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
 // put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
@@ -73,3 +73,75 @@ LRUCache.prototype.put = function(key, value) {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
+
+
+// 2) Map + Double linked list
+class DLinkedNode {
+  constructor(key, value) {
+    this.key = key
+    this.value = value
+    this.pre = null
+    this.post = null
+  }
+}
+class LRUCache {
+  constructor(capacity) {
+    this.capacity = capacity
+    this.cache = new Map()
+    this.count = 0
+    this.head = new DLinkedNode()
+    this.head.pre = null
+    this.tail = new DLinkedNode()
+    this.tail.post = null
+    this.head.post = this.tail
+    this.tail.pre = this.head
+  }
+
+  addNode(node) {
+    node.pre = this.head
+    node.post = this.head.post
+    this.head.post.pre = node
+    this.head.post = node
+  }
+  removeNode(node) {
+    let pre = node.pre
+    let post = node.post
+    pre.post = post
+    post.pre = pre
+  }
+  moveToHead(node) {
+    this.removeNode(node)
+    this.addNode(node)
+  }
+  popTail() {
+    let res = this.tail.pre
+    this.removeNode(res)
+    return res
+  }
+
+  get(key) {
+    let node = this.cache.get(key)
+    if (node === undefined) {
+      return -1
+    }
+    this.moveToHead(node)
+    return node.value
+  }
+  put(key, value) {
+    let node = this.cache.get(key)
+    if (node === undefined) {
+      let newNode = new DLinkedNode(key, value)
+      this.cache.set(key, newNode)
+      this.addNode(newNode)
+      ++this.count
+      if (this.count > this.capacity) {
+        let tail = this.popTail()
+        this.cache.delete(tail.key)
+        --this.count
+      }
+    } else {
+      node.value = value
+      this.moveToHead(node)
+    }
+  }
+}
